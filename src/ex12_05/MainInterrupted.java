42835c73de46de38fee5a5e7c8b01d71a07db23c
a71143055 <a71143055@gmail.com>
2024-11-28 10:26:47 +0900

    java1128

---
+++ src/ex12_05/MainInterrupted.java
@@ -0,0 +1,9 @@
+package ex12_05;
+
+public class MainInterrupted {
+    public static void main(String[] args) {
+        InterruptedExceptionTest interrupted =new InterruptedExceptionTest();
+
+        interrupted.printDelay();
+    }
+}
